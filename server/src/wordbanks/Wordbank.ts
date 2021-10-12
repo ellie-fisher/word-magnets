ed8eb3a31b32e75bf044e762002ee5698c4d040d
ellie-fisher <ellie.am.fisher@gmail.com>
2021-10-11 23:12:15 -0700

    Add `partsOfSpeech` property, `isFixed`, `setWords()`, and `getWord()`

--- server/src/wordbanks/Wordbank.ts
+++ server/src/wordbanks/Wordbank.ts
@@ -1,12 +1,24 @@
 class Wordbank
 {
 	public displayName: string;
+	public partsOfSpeech: string[];
 	protected _words: string[];
 
-	constructor ( displayName: string, words: string[] )
+	constructor ( displayName: string, partsOfSpeech: string[], words: string[] = [] )
 	{
 		this.displayName = displayName;
+		this.partsOfSpeech = partsOfSpeech;
 		this._words = words;
+	}
+
+	setWords ( words: string[] )
+	{
+		this._words = words;
+	}
+
+	getWord ( index: number ): string
+	{
+		return this.isValidWord (index) ? this._words[index] : "";
 	}
 
 	isValidWord ( index: number ): boolean
@@ -21,6 +33,11 @@ class Wordbank
 	{
 		substitutes = substitutes.slice ();
 		// TODO: Filter words with to-be-created rules.
+	}
+
+	get isFixed ()
+	{
+		return this.partsOfSpeech.length <= 0;
 	}
 }
 
