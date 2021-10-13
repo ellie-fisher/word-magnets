33c71bc114bd10f328e1e6669134d7e100e9abc8
ellie-fisher <ellie.am.fisher@gmail.com>
2021-10-12 17:02:36 -0700

    Add `Wordbanks` packet command

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -26,6 +26,7 @@ const commands =
 	"ClientList",
 	"RoomList",
 
+	"Wordbanks",
 	"SendSentence",
 	"SentenceList",
 
--- server/src/packets/PacketCommand.ts
+++ server/src/packets/PacketCommand.ts
@@ -24,6 +24,7 @@ enum PacketCommand
 	ClientList,
 	RoomList,
 
+	Wordbanks,
 	SendSentence,
 	SentenceList,
 
