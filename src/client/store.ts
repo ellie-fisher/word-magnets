9ebf544386b39ce8420255d2ad103fca355564c2
ellie-fisher <ellie.am.fisher@gmail.com>
2022-01-25 22:54:58 -0800

    Add Redux DevTools

--- src/client/store.ts
+++ src/client/store.ts
@@ -3,7 +3,12 @@ import { createStore } from "redux";
 import rootReducer from "./reducer";
 
 
-const store = createStore (rootReducer);
+const store = createStore (
+	rootReducer,
+	// @ts-ignore
+	// TODO: Only enable in development mode.
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ (),
+);
 
 
 export default store;
