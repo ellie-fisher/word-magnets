8cee16aa25b8f2afbe3df9f869b69b3c3f8fd156
ellie-fisher <ellie.am.fisher@gmail.com>
2022-01-26 00:07:11 -0800

    Add basic tab switching

--- src/client/mainMenu/MainMenu.tsx
+++ src/client/mainMenu/MainMenu.tsx
@@ -6,6 +6,8 @@ import MainMenuTab from "./MainMenuTab";
 import CreateRoom from "./createRoom/CreateRoom";
 import JoinRoom from "./joinRoom/JoinRoom";
 
+import MainMenuActions from "./actionCreators";
+
 import { AnyObject } from "../../common/util/types";
 
 
@@ -19,7 +21,21 @@ class MainMenu extends Component<MainMenuProps>
 
 		return (
 			<div>
-				{props.tab === MainMenuTab.CreateRoom ? <CreateRoom /> : <JoinRoom />}
+				<div>
+					<button onClick={( event ) => props.selectTab (MainMenuTab.CreateRoom)}>
+						Create Room
+					</button>
+
+					<button onClick={( event ) => props.selectTab (MainMenuTab.JoinRoom)}>
+						Join Room
+					</button>
+				</div>
+
+				<hr />
+
+				<div>
+					{props.tab === MainMenuTab.CreateRoom ? <CreateRoom /> : <JoinRoom />}
+				</div>
 			</div>
 		);
 	}
@@ -32,5 +48,15 @@ const mapStateToProps = state =>
 	};
 };
 
+const mapDispatchToProps = dispatch =>
+{
+	return {
+		selectTab ( tab: MainMenuTab )
+		{
+			dispatch (MainMenuActions.selectTab (tab));
+		},
+	};
+};
 
-export default connect (mapStateToProps) (MainMenu);
+
+export default connect (mapStateToProps, mapDispatchToProps) (MainMenu);
