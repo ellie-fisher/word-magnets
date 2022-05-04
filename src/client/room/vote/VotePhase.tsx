fb51c009c0719ad999c190ce3580ce2b66014fb7
ellie-fisher <ellie.am.fisher@gmail.com>
2022-05-04 11:09:03 -0700

    Disable vote buttons on phase end

--- src/client/room/vote/VotePhase.tsx
+++ src/client/room/vote/VotePhase.tsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import Table from "../Table";
+
+import RoomPhaseState from "../../../common/rooms/phases/RoomPhaseState";
 import VotePhaseActions from "./actionCreators";
 
 import { AnyObject } from "../../../common/util/types";
@@ -33,6 +35,7 @@ class VotePhase extends Component<VotePhaseProps, AnyObject>
 								<button
 									key={`player-sentence-btn-${sentence.voteID}`}
 									onClick={() => props.setVote (sentence.voteID)}
+									disabled={props.phase.state === RoomPhaseState.End}
 								>
 									Vote
 								</button>,
@@ -50,6 +53,7 @@ class VotePhase extends Component<VotePhaseProps, AnyObject>
 const mapStateToProps = state =>
 {
 	return {
+		phase: state.room.general.phase,
 		sentences: state.room.vote.sentences,
 		voteID: state.room.vote.voteID,
 	};
