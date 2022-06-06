720ff541b4bc76cec32690d63e34da2c16a2d508
ellie-fisher <ellie.am.fisher@gmail.com>
2022-06-06 14:19:09 -0700

    Add show/hide and copy for room ID
    + Add `copy-to-clipboard` npm package

--- src/client/room/Topbar.tsx
+++ src/client/room/Topbar.tsx
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import copy from "copy-to-clipboard";
+
 import RoomActions from "./general/actionCreators";
 
 import { AnyObject } from "../../common/util/types";
 
 
 type TopbarProps = AnyObject;
+type TopbarLocalState = AnyObject;
 
-class Topbar extends Component<TopbarProps>
+class Topbar extends Component<TopbarProps, TopbarLocalState>
 {
+	constructor ( props )
+	{
+		super (props);
+
+		this.state =
+		{
+			showID: false,
+			copiedID: false,
+		};
+	}
+
+	clickCopy ( id: string )
+	{
+		copy (id);
+
+		this.setState ({ copiedID: true });
+		setTimeout (() => this.setState ({ copiedID: false }), 750);
+	}
+
+	clickID ()
+	{
+		this.setState ({ showID: !this.state.showID });
+	}
+
 	render ()
 	{
-		const { props } = this;
+		const { props, state } = this;
 		const { info } = props;
 
-		const headerStyle = { padding: "1em" };
+		const roomIDStyle: AnyObject =
+		{
+			fontFamily: "monospace",
+			cursor: "pointer",
+		};
+
+		if ( state.showID )
+		{
+			roomIDStyle.userSelect = "text";
+		}
 
 		return (
 			<div className="topbar">
@@ -23,14 +59,24 @@ class Topbar extends Component<TopbarProps>
 					{"<< Leave Room"}
 				</button>
 
-				<span style={headerStyle}>Time Left: {info.timeLeft}</span>
-				<span style={headerStyle}>Round: {info.currentRound} of {info.maxRounds}</span>
+				<span className="topbar-item">Time Left: {info.timeLeft}</span>
+				<span className="topbar-item">Round: {info.currentRound} of {info.maxRounds}</span>
 
-				<span style={headerStyle}>
+				<span className="topbar-item">
 					<strong>ID: </strong>
 
-					<span style={{ fontFamily: "monospace", userSelect: "text" }}>
-						{info.id}
+					<span style={roomIDStyle} onClick={() => this.clickID ()}>
+						{state.showID ? info.id : info.id?.replace (/./g, "•")}
+					</span>
+
+					<span style={{ padding: "0.5em" }}>
+						<button
+							className={`dashed btn orange ${state.copiedID ? "disabled" : ""}`}
+							onClick={() => this.clickCopy (info.id)}
+							disabled={state.copiedID}
+						>
+							{state.copiedID ? "Copied" : "Copy"}
+						</button>
 					</span>
 				</span>
 			</div>
