98c0305ce83c679f2100b7ba513cee8b36835622
Ellie Fisher <ellie.am.fisher@gmail.com>
2025-01-19 19:52:14 -0800

    Packet validation test refactor

--- test/2. Packet Validation/test.ts
+++ test/2. Packet Validation/test.ts
@@ -1,10 +1,34 @@
-import { InvalidTest } from "./Invalid";
-import { ClientIDTest } from "./ClientID";
-import { CreateRoomTest } from "./CreateRoom";
+import { PacketType } from "../../src/common/packets/PacketType";
+import { RoomFields } from "../../src/common/fields/fields";
+import { validate } from "./validate";
+
+const { fields: roomFields } = RoomFields;
 
 describe("Packet Validation", function()
 {
-	it("should pack and unpack `Invalid` packets properly", InvalidTest);
-	it("should pack and unpack `ClientID` packets properly", ClientIDTest);
-	it("should pack and unpack `CreateRoom` packets properly", CreateRoomTest);
+	validate("Invalid", [PacketType.Invalid], {});
+	validate("ClientID", [PacketType.ClientID, "test-id"], { id: "test-id" });
+
+	validate(
+		"CreateRoom",
+		[PacketType.CreateRoom, "Room Creator", 90, 8, 10],
+		{
+			clientData: { name: "Room Creator" },
+			roomData:
+			{
+				timeLimit: 90,
+				maxRounds: 8,
+				maxPlayers: 10,
+			},
+		},
+		{
+			clientData: { name: "" },
+			roomData:
+			{
+				timeLimit: roomFields.timeLimit.default,
+				maxRounds: roomFields.maxRounds.default,
+				maxPlayers: roomFields.maxPlayers.default,
+			},
+		},
+	);
 });
---
+++ test/2. Packet Validation/validate.ts
@@ -0,0 +1,28 @@
+import { deepStrictEqual, notEqual } from "node:assert";
+
+import { Packet } from "../../src/common/packets/Packet";
+import { RawPacket } from "../../src/common/packets/types";
+import { AnyObject } from "../../src/common/util";
+
+export function validate(type: string, raw: RawPacket, template: AnyObject, defaultValues: AnyObject | null = null)
+{
+	it(`should pack and unpack \`${type}\` packets properly`, function()
+	{
+		const backup = [...raw];
+		const packet = Packet.fromArray(raw);
+
+		deepStrictEqual(Packet.toArray(raw[0], packet), raw);
+
+		/* Make sure `raw` isn't modified. */
+		deepStrictEqual(backup, raw);
+		notEqual(Packet.toArray(raw[0], packet), raw);
+
+		// Test field values against `template`.
+		deepStrictEqual(packet, template);
+
+		if (defaultValues !== null)
+		{
+			deepStrictEqual(Packet.fromArray([raw[0]]), defaultValues);
+		}
+	});
+};
