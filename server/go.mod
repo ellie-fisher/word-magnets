d216fc488e5124853a666664f28cbbc3dac0c1bf
Ellie Fisher <ellie.am.fisher@gmail.com>
2025-07-21 00:23:50 -0700

    Initial commit

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module word-magnets
+
+go 1.24.2
---
+++ server/main.go
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2025 Ellie Fisher
+ *
+ * This file is part of the Word Magnets source code. It may be used under the GNU Affero General Public License v3.0.
+ *
+ * For full terms, see the LICENSE file or visit https://spdx.org/licenses/AGPL-3.0-or-later.html
+ */
+
+package main
+
+import "log"
+
+func main() {
+	log.Printf("It works!")
+}
